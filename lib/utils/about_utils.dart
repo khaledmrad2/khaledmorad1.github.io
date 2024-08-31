bf20a0882c57c2d9d9c92d5437d0686632f4ebcc
khaled.m@sawaftech.com <khaled.m@sawaftech.com>
2024-08-31 18:26:06 +0300

    test khaled

--- lib/utils/about_utils.dart
+++ lib/utils/about_utils.dart
@@ -2,6 +2,7 @@ class AboutUtils {
   static const String aboutMeHeadline =
       'I\'m Khaled Morad, a Software Engineer, Flutter developer ,and passionate Problem Solver .';
 
+
   static const String aboutMeDetail =
       'I\'m a Software Engineer Graduate with Information Technologies engineering as Majors from Damascus University, Syria. I have been developing mobile apps for over 2 years now. I have worked in teams for various startups and helped them in launching their prototypes and got valuable learning experience. Been a Contestant and Coach in ACM Contest with More than 1K Problem Solved on Codeforces in Damascus Univercity';
 }
